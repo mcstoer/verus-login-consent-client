af73a9dfae44207c1ba62159a4f205a2ab872b17
mcstoer <49734282+mcstoer@users.noreply.github.com>
2025-04-21 09:04:50 -0700

    Change the Login component to a functional component.

--- src/components/LoginConsent/Login/Login.js
+++ src/components/LoginConsent/Login/Login.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { setExternalAction, setNavigationPath } from '../../../redux/reducers/navigation/navigation.actions';
-import { 
-  LoginRender
-} from './Login.render';
 import {
   CONSENT_TO_SCOPE,
   EXTERNAL_ACTION,
   EXTERNAL_CHAIN_START,
   REDIRECT,
   PROVISIONING_FORM
-} from '../../../utils/constants'
+} from '../../../utils/constants';
 import { checkAndUpdateIdentities, setActiveVerusId } from '../../../redux/reducers/identity/identity.actions';
 import { signResponse } from '../../../rpc/calls/signResponse';
 import { setError } from '../../../redux/reducers/error/error.actions';
@@ -21,124 +18,258 @@ import {
 } from 'verus-typescript-primitives';
 import BigNumber from 'bignumber.js';
 import { getCredentialsByScope } from '../../../rpc/calls/getCredentials';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { VerusIdLogo } from "../../../images";
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
+const Login = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const { canLoginOrGiveConsent, setRequestResult } = props;
+  const dispatch = useDispatch();
+  const { request } = useSelector((state) => state.rpc.loginConsentRequest);
+  const [loading, setLoading] = useState(false);
+  const identities = useSelector((state) => state.identity.identities);
+  const activeIdentity = useSelector((state) => state.identity.activeIdentity);
 
-    this.state = {
-      loading: false
+  // See if the webhook exists.
+  let canProvision = request.challenge.provisioning_info && request.challenge.provisioning_info.some(x => {
+    return (
+      x.vdxfkey === LOGIN_CONSENT_ID_PROVISIONING_WEBHOOK_VDXF_KEY.vdxfid
+    );
+  });
+
+  // Provisioning is not an option if the subject is specified to be one of the identities that the user owns.
+  if (identities.length > 0) {
+    const identitySubjects =
+      request.challenge.subject.filter(item => item.vdxfkey === ID_ADDRESS_VDXF_KEY.vdxfid).map(id => id.data);
+
+    const identitySubjectMatches = identities.filter(id => identitySubjects.includes(id.identity.identityaddress));
+
+    if (identitySubjectMatches.length > 0) {
+      canProvision = false;
     }
-
-    // Check to see if provisioning is an option.
-    const { request } = this.props.loginConsentRequest;
-
-    // See if the webhook exists.
-    let canProvision = request.challenge.provisioning_info && request.challenge.provisioning_info.some(x => {
-      return (
-        x.vdxfkey === LOGIN_CONSENT_ID_PROVISIONING_WEBHOOK_VDXF_KEY.vdxfid
-      );
-    });
-
-    // Provisioning is not an option if the subject is specified to be one of the identities that the user owns.
-    if (this.props.identities.length > 0) {
-      const identitySubjects = 
-        request.challenge.subject.filter(item => item.vdxfkey === ID_ADDRESS_VDXF_KEY.vdxfid).map(id => id.data);
-
-      const identitySubjectMatches = this.props.identities.filter(id => identitySubjects.includes(id.identity.identityaddress));
-
-      if (identitySubjectMatches.length > 0) {
-        canProvision = false;
-      }
-    }
-
-    this.canProvision = canProvision;
-    this.tryLogin = this.tryLogin.bind(this);
-    this.tryProvision = this.tryProvision.bind(this);
-    this.selectId = this.selectId.bind(this);
-    this.cancel = this.cancel.bind(this);
   }
 
-  tryLogin() {
-    this.setState({ loading: true }, async () => {
-      const { request } = this.props.loginConsentRequest
-      const userActions = await checkAndUpdateIdentities(request.chainTicker)
-      userActions.map(action => this.props.dispatch(action))
+  const cancel = () => {
+    dispatch(setNavigationPath(CONSENT_TO_SCOPE));
+  };
 
-      if (this.props.canLoginOrGiveConsent()) {
+  const tryLogin = async() => {
+    setLoading(true);
 
-        const loginIdentity = this.props.activeIdentity.identity.identityaddress;
+    const userActions = await checkAndUpdateIdentities(request.chainTicker);
+    userActions.map(action => dispatch(action));
 
-        try {
-          // Get the associated credentials based on the signing id.
-          const credentials = await getCredentialsByScope(
-            request.chainTicker,
-            loginIdentity,
-            request.signedBy.identity.identityaddress
-          );
+    if (canLoginOrGiveConsent()) {
+      const loginIdentity = activeIdentity.identity.identityaddress;
 
-          let response = new LoginConsentResponse({
-            system_id: request.system_id,
-            signing_id: loginIdentity,
-            decision: new LoginConsentDecision({
-              decision_id: request.challenge.challenge_id,
-              request: request,
-              created_at: BigNumber(Date.now())
-                .dividedBy(1000)
-                .decimalPlaces(0)
-                .toNumber(),
-              credentials: credentials,
-            })
-          });
-  
-          response.chainTicker = request.chainTicker
-          
-          const sigRes = await signResponse(response);
-          
-          this.props.setRequestResult(sigRes, () => {
-            this.props.dispatch(setNavigationPath(REDIRECT))
+      try {
+        // Get the associated credentials based on the signing id.
+        const credentials = await getCredentialsByScope(
+          request.chainTicker,
+          loginIdentity,
+          request.signedBy.identity.identityaddress
+        );
+
+        let response = new LoginConsentResponse({
+          system_id: request.system_id,
+          signing_id: loginIdentity,
+          decision: new LoginConsentDecision({
+            decision_id: request.challenge.challenge_id,
+            request: request,
+            created_at: BigNumber(Date.now())
+              .dividedBy(1000)
+              .decimalPlaces(0)
+              .toNumber(),
+            credentials: credentials,
           })
-        } catch(e) {
-          this.props.dispatch(setError(e))
-        }
-      } else {
-        this.props.dispatch(setExternalAction(EXTERNAL_CHAIN_START))
-        this.props.dispatch(setNavigationPath(EXTERNAL_ACTION));
+        });
+
+        response.chainTicker = request.chainTicker;
+        
+        const sigRes = await signResponse(response);
+        
+        setRequestResult(sigRes, () => {
+          dispatch(setNavigationPath(REDIRECT));
+        });
+      } catch(e) {
+        setLoading(false);
+        dispatch(setError(e));
       }
-    })
-  }
+    } else {
+      dispatch(setExternalAction(EXTERNAL_CHAIN_START));
+      dispatch(setNavigationPath(EXTERNAL_ACTION));
+    }
+  };
 
-  tryProvision() {
-    this.props.dispatch(setNavigationPath(PROVISIONING_FORM));
-  }
+  const tryProvision = () => {
+    dispatch(setNavigationPath(PROVISIONING_FORM));
+  };
 
-  cancel() {
-    this.props.dispatch(setNavigationPath(CONSENT_TO_SCOPE));
-  }
-
-  selectId(address) {
-    this.props.dispatch(
+  const selectId = (address) => {
+    dispatch(
       setActiveVerusId(
         address.length == 0
           ? null
-          : this.props.identities.find((x) => address === x.identity.identityaddress)
+          : identities.find((x) => address === x.identity.identityaddress)
       )
     );
-  }
-
-  render() {
-    return LoginRender.call(this);
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    path: state.navigation.path,
-    loginConsentRequest: state.rpc.loginConsentRequest,
-    identities: state.identity.identities,
-    activeIdentity: state.identity.activeIdentity,
-    originApp: state.origin.originApp
   };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        flex: 1,
+        height: "100%",
+      }}
+    >
+      <div
+        style={{
+          height: "100%",
+          display: "flex",
+          padding: 32,
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <img src={VerusIdLogo} width={'55%'} height={'10%'}/>
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "flex-start",
+          }}
+        >
+          <div
+            style={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "row",
+              padding: 8,
+            }}
+          >
+            {`Select an Identity` +
+              (canProvision ? " or Request an Identity" : "")}
+          </div>
+        </div>
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "flex-start",
+            justifyContent: "center",
+            flex: 1,
+            paddingTop: 2,
+          }}
+        >
+          <FormControl style={{ maxWidth: 560, flex: 1 }}>
+            <Select
+              value={
+                activeIdentity == null
+                  ? ""
+                  : activeIdentity.identity.identityaddress
+              }
+              displayEmpty
+              inputProps={{ "aria-label": "Select a VerusID" }}
+              style={{
+                textAlign: "start",
+                paddingTop: 2,
+              }}
+              onChange={(e) => {
+                return selectId(e.target.value);
+              }}
+            >
+              <MenuItem value="">
+                <em>Select a VerusID</em>
+              </MenuItem>
+              {identities.map((id, index) => {
+                return (
+                  <MenuItem
+                    key={index}
+                    value={id.identity.identityaddress}
+                  >{`${id.identity.name}@`}</MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+        </div>
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "column-reverse",
+            alignItems: "center",
+            justifyContent: "flex-end",
+            flex: 1,
+          }}
+        >
+          {canProvision && <Button
+            variant="contained"
+            color="primary"
+            disabled={loading}
+            onClick={() => tryProvision()}
+            style={{
+              width: 240,
+              padding: 8,
+            }}
+          >
+            {"Request a new VerusID"}
+          </Button>}
+        </div>
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "flex-end",
+            justifyContent: "flex-end",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Button
+              variant="text"
+              disabled={loading}
+              color="secondary"
+              onClick={() => cancel()}
+              style={{
+                width: 120,
+                marginRight: 32,
+                padding: 8,
+              }}
+            >
+              {"Back"}
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              disabled={loading || activeIdentity == null}
+              onClick={() => tryLogin()}
+              style={{
+                width: 120,
+                padding: 8,
+              }}
+            >
+              {"Continue"}
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default connect(mapStateToProps)(Login);
+export default Login;
