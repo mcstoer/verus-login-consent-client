a2647326138178520c217109f7573639052ebe8a
mcstoer <49734282+mcstoer@users.noreply.github.com>
2025-05-08 14:52:59 -0700

    Change adding credentials to the login response to use the decision's context.

--- src/utils/loginResponse.js
+++ src/utils/loginResponse.js
@@ -1,5 +1,7 @@
 import BigNumber from 'bignumber.js';
 import { 
+  Context,
+  Credential,
   LoginConsentDecision,
   LoginConsentResponse
 } from 'verus-typescript-primitives';
@@ -7,17 +9,24 @@ import { signResponse } from '../rpc/calls/signResponse';
 
 // Creates a LoginConsentResponse using a LoginConsentRequest and signs it by calling the main app.
 export const createAndSignLoginResponse = async (request, loginIdentity, credentials) => {
+
+  const context = new Context();
+  for (const cred of credentials) {
+    const c = new Credential(cred);
+    context.kv[cred.credentialKey] = c.toBuffer().toString('hex');
+  }
+
   let response = new LoginConsentResponse({
     system_id: request.system_id,
     signing_id: loginIdentity,
     decision: new LoginConsentDecision({
       decision_id: request.challenge.challenge_id,
+      context: context,
       request: request,
       created_at: BigNumber(Date.now())
         .dividedBy(1000)
         .decimalPlaces(0)
         .toNumber(),
-      credentials: credentials,
     })
   });
 
